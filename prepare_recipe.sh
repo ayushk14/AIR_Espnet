1cdb656885a3e8038c4bec867eb06785ff231c66
Ayush Khopkar <khopkar.ayush10@gmail.com>
2019-04-29 18:02:55 +0530

    adding files

---
+++ prepare_recipe.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#GIVE ALL SHELL SCRIPTS EXECUTABLE PERMISSION IN STARTING
+
+export USER=$(whoami)
+
+recipe_name="timit"
+
+rm ~/espnet/egs/$recipe_name/asr1/run.sh
+cp -a Recipe_Files/run.sh ~/espnet/egs/$recipe_name/asr1/run.sh
+
+rm ~/espnet/egs/$recipe_name/asr1/steps/make_mfcc.sh
+cp -a Recipe_Files/make_mfcc.sh ~/espnet/egs/$recipe_name/asr1/steps/make_mfcc.sh
+
+cp -a Recipe_Files/local_custom ~/espnet/egs/$recipe_name/asr1/local_custom
+cp -a Recipe_Files/Python_Files ~/espnet/egs/$recipe_name/asr1/Python_Files
+cp -a Recipe_Files/steps_fa ~/espnet/egs/$recipe_name/asr1/steps_fa
+
+data_dir=~/espnet/egs/$recipe_name/asr1/data/local/data
+dict_dr=~/espnet/egs/$recipe_name/asr1/data/local/dict
+
+mkdir -p $data_dir $dict_dir
+
+cp -a Recipe_Files/uttids/train.uttids ~/espnet/egs/$recipe_name/asr1/data/local/data/train.uttids
+cp -a Recipe_Files/uttids/test.uttids ~/espnet/egs/$recipe_name/asr1/data/local/data/test.uttids
+cp -a Recipe_Files/uttids/dev.uttids ~/espnet/egs/$recipe_name/asr1/data/local/data/dev.uttids
+
+echo "Receipe is updated with required files"
